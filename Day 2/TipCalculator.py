1ee1a05406ff343d4995498950153f07da196b66
eazeta <emmanuel.2000z@gmail.com>
2024-02-13 00:06:04 +0000

    minor change

--- Day 2/TipCalculator.py
+++ Day 2/TipCalculator.py
@@ -1,10 +1,11 @@
 #Introduction
 print("Welcome to the Tip Calculator!")
 #Initialised variables
-total = int(input("What was the total bill? £"))
+total = float(input("What was the total bill? £"))
 split = int(input("How many people would like to split the bill? "))
 percentage = int(input("What percentage tip would you like to give? 10%, 12% or 15%? "))
 #Calculation
 payment = (total + ((total * percentage)/100)) / split
+payment = round(payment,2)
 #Output
 print("Each person should pay £" + str(payment))
