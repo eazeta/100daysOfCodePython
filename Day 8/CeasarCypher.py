db558dc9e31fa189cf66ba4c529294d823690c39
eazeta <emmanuel.2000z@gmail.com>
2024-02-19 19:30:34 +0000

    Caesar Cypher

---
+++ Day 8/CeasarCypher.py
@@ -0,0 +1,47 @@
+#Ceasar Cyper shifts the alphabet by a certain amount
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#Function to encrypt message
+def encode(message, shift):
+
+  encryptedMessage = []
+
+  listMessage = list(message)
+  for letter in listMessage:
+    position = alphabet.index(letter)
+    newposition = position + shift
+    if newposition > 25:
+      newposition -= 26
+    encryptedMessage.append(alphabet[newposition])
+  
+  print(''.join(encryptedMessage))
+
+#Function to decrypt message
+def decode(message, shift):
+
+  decryptedMessage = []
+
+  listMessage = list(message)
+  for letter in listMessage:
+    position = alphabet.index(letter)
+    newposition = position - shift
+    if newposition < 1:
+      newposition += 26
+    decryptedMessage.append(alphabet[newposition])
+  
+  print(''.join(decryptedMessage))
+
+  
+
+message = input("Enter your message: \n").lower()
+shift = int(input("Enter your shift: \n"))
+
+toDo = input("Enter 'e' for Encryption and 'd' for Decryption: ").lower()
+
+if toDo == 'e':
+  encode(message, shift)
+elif toDo == 'd':
+  decode(message, shift)
+else:
+  print("error")
